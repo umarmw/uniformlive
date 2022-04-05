579b1c8e26d71808f010b1acf9dee22c58f49e06
Umar Waliyullah <umar.mw@gmail.com>
2022-04-05 15:01:50 +0400

    live reload

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,11 @@ import {
 import { Composition, Slot } from "@uniformdev/canvas-react";
 import resolveRenderer from "../lib/resolveRenderer";
 
+// LESSON 7 - ACTIVITY 6- START
+import { useLivePreviewNextStaticProps } from "../hooks/useLivePreviewNextStaticProps";
+import getConfig from "next/config";
+// LESSON 7 - ACTIVITY 6 - END
+
 export async function getStaticProps({ preview }) {
   const client = new CanvasClient({
     apiKey: process.env.UNIFORM_API_KEY,
@@ -24,7 +29,18 @@ export async function getStaticProps({ preview }) {
   };
 }
 
+// LESSON 7 - ACTIVITY 6 - START
+const { publicRuntimeConfig } = getConfig();
+const { uniform } = publicRuntimeConfig;
+// LESSON 7 - ACTIVITY 6 - END
+
 export default function Home({ composition }) {
+  // LESSON 7 - ACTIVITY 6 - START
+  useLivePreviewNextStaticProps({
+    compositionId: composition?._id,
+    projectId: uniform.projectId,
+  });
+  // LESSON 7 - ACTIVITY 6 - END
   return (
     <Composition data={composition} resolveRenderer={resolveRenderer}>
       <div className={styles.container}>
