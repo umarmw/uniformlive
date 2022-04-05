d2db3a8ac9412ee31eeb3ea1e93b425790c98193
Umar Waliyullah <umar.mw@gmail.com>
2022-04-05 14:08:49 +0400

    initial repo

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Footer = () => {
+  return <div>My site &copy; {new Date().getFullYear()}</div>;
+};
---
+++ components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export const Header = ({ component }) => {
+  return <h1 className={styles.title}>Welcome!</h1>;
+};
